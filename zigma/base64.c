4151955cf091936b9c901c0fbc9c1219b823e6ae
Chase Zehl O'Byrne <zehl@live.com>
2023-12-09 07:02:42 -0500

    Refactor base64 decoding table and buffer sanitization logic

--- zigma/base64.c
+++ zigma/base64.c
@@ -32,11 +32,6 @@ static const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                    "abcdefghijklmnopqrstuvwxyz"
                                    "0123456789+/";
 
-static const char base64_decoding_table[] = {
-    62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0,  1,  2,  3,  4,
-    5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1,
-    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
-
 unsigned int base64_encode(char* data, char const* buffer, unsigned long length)
 {
   unsigned long output_length = 4 * ((length + 2) / 3);
@@ -110,7 +105,7 @@ unsigned int buffer_sanitize(char* output, char const* input, unsigned long leng
       continue;
     }
 
-    if (!in_comment) {
+    if (!in_comment && ch != ' ' && ch != '\t') {
       output[output_length++] = ch;
     }
   }
